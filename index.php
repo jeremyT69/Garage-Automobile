48918227bb8673236dd5ad86ba9ba0439f1dda33
jeremyT69 <jeremy.thomas69@outlook.fr>
2023-06-05 17:54:27 +0200

    creation index.php, page style.css et ajout Logo

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--Bootstrap-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!--Fontawesome-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <!--Css-->
+  <link rel="stylesheet" href="css/style.css">
+  <title>Garage V.Parrot</title>
+</head>
+
+<body>
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <div class="col-md-3 mb-2 mb-md-0">
+        <img class="img" src="logo.png" alt="logo du garage">
+      </div>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2">Acceuil</a></li>
+        <li><a href="#" class="nav-link px-2">Carrosserie</a></li>
+        <li><a href="#" class="nav-link px-2">Mécanique</a></li>
+        <li><a href="#" class="nav-link px-2">Véhicules d'occassions</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+      </div>
+    </header>
+  
+</body>
+</html>
